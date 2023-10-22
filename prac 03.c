4c501a2efea6a4b622f8fd953c8530b031239e41
Shaswata Chowdhury <140137696+shaswatacp@users.noreply.github.com>
2023-10-23 02:57:23 +0600

    Add files via upload

---
+++ prac 03.c
@@ -0,0 +1,13 @@
+//A c program to print all even numbers between 1 to '100' using while loop.
+#include<stdio.h>
+int main()
+{
+    int i=2;
+    while(i<=100)
+    {
+        printf("%d\n",i);
+        i+=2;
+    }
+    return 0;
+
+}
