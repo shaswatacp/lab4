f4f7786427b4b6f6517a670dbb9ce99bbd4fcc17
Shaswata Chowdhury <140137696+shaswatacp@users.noreply.github.com>
2023-10-22 19:18:44 +0600

    Add files via upload

---
+++ example 01.c
@@ -0,0 +1,11 @@
+//A program to print the first ten numbers using for loop.
+#include<stdio.h>
+int main()
+{
+    int i;
+    for(i=1;i<=10;++i)
+    {
+        printf("%d\n",i);
+    }
+    return 0;
+}
