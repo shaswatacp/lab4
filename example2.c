14eea3f5e926c76b1087e4238d3e17cc82805272
Shaswata Chowdhury <140137696+shaswatacp@users.noreply.github.com>
2023-10-23 00:54:21 +0600

    Add files via upload

---
+++ example2.c
@@ -0,0 +1,15 @@
+//A c program to calculate the sum of first 'n' natural numbers.
+#include<stdio.h>
+int main()
+{
+    int num,count,sum=0;
+    printf("Enter an integer  :  ");
+    scanf("%d",&num);
+
+    for(count=1;count<=num;count++)
+    {
+        sum+=count;
+    }
+    printf("sum=%d",sum);
+    return 0;
+}
